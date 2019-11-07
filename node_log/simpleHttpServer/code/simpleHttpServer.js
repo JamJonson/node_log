47457b3828632e904961390ca7dfc69b6a59af22
cqj_9412 <978390012@qq.com>
2019-11-07 17:42:16 +0800

    完善请求

--- node_log/simpleHttpServer/code/simpleHttpServer.js
+++ node_log/simpleHttpServer/code/simpleHttpServer.js
@@ -4,6 +4,10 @@ var http = require('http')
 var server = http.createServer()
 server.on('request', function(request, response){
 	console.log('接受到用户的请求,请求路径是：', request.url)
+	// 可以多个write
+	response.write('hellow')
+	response.write('node.js')
+	response.end() // 必须end，不然客户端不会有响应
 })
 server.listen(3000, function (){
 	console.log('服务器启动成功, 通过访问 http://127.0.0.1:3000/ 进行访问')
